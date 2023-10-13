b966460144aae3aaf1079b5899726e5ed60dd856
TelmoLewis <124844690+TelmoLewis@users.noreply.github.com>
2023-10-13 15:24:11 +0100

    Add files via upload

---
+++ selection_part1.py
@@ -0,0 +1,20 @@
+
+
+age = 19
+
+if age >= 18:
+    print("you are in category A")
+if age >= 16:
+    print("you are in category B")
+if age < 16:
+    print("you are in category C")  
+        
+
+person =
+
+if person > 18:
+    print("You are over 18 years old")
+elif person >= 16:
+    print("You are 16 or over")
+else:
+    print("you are not elegible")        
