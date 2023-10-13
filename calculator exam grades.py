1e06ff2f7d7dc993ad40b0f506b956065fe49ae5
TelmoLewis <124844690+TelmoLewis@users.noreply.github.com>
2023-10-13 15:53:47 +0100

    Update calculator exam grades.py

--- calculator exam grades.py
+++ calculator exam grades.py
@@ -9,3 +9,34 @@ elif grade > 60 and grade <= 70:
     print("Merit")
 elif grade > 70 and grade >= 100:
     print("Distinction")
+
+
+
+# part 2
+
+
+mark = int(input("Enter exam mark (between 1 and 100): "))
+level = int(input("Enter student level (1 or 2): "))
+
+if mark < 1 or mark > 100:
+    print("Error: marks must be between 1 and 100")
+elif level == 1:
+    if mark < 50:
+        print("Fail")
+    elif mark <= 60:
+        print("Pass")
+    elif mark <= 70:
+        print("Merit")
+    else:
+        print("Distinction")
+elif level == 2:
+    if mark < 40:
+        print("Fail")
+    elif mark <= 50:
+        print("Pass")
+    elif mark <= 65:
+        print("Merit")
+    else:
+        print("Distinction")
+else:
+    print("Invalid level, please enter 1 or 2")
