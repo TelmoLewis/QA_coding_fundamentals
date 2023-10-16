7838102435d1221abe19c1f893b507813b8fcc9c
TelmoLewis <124844690+TelmoLewis@users.noreply.github.com>
2023-10-16 14:18:34 +0100

    Update ExamAverage.py

--- ExamAverage.py
+++ ExamAverage.py
@@ -2,19 +2,19 @@ math_mark = 0
 english_mark = 0
 ict_mark = 0
 
-while math_mark <= 0 or math_mark >= 100:
+while math_mark < 0 or math_mark > 100:
     math_mark = int(input("enter your maths mark between 0 and 100: "))
     if math_mark < 0 or math_mark > 100:
         print("Invalid input, marks should be between 1 and 100")
     
 
-while english_mark <= 0 or english_mark >= 100:
+while english_mark < 0 or english_mark > 100:
     english_mark = int(input("enter your english mark between 0 and 100: "))
     if english_mark < 0 or english_mark > 100:
         print("Invalid input, marks should be between 1 and 100")
  
         
-while ict_mark <= 0 or ict_mark >= 100:
+while ict_mark < 0 or ict_mark > 100:
     ict_mark = int(input("enter your ict mark between 0 and 100: "))
     if ict_mark < 0 or ict_mark > 100:
         print("Invalid input, marks should be between 1 and 100")
