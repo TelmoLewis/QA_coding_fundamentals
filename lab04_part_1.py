f7fefdf6eb6a288d401691d8d0aadfbffb40b7de
telmo <telmo.lewisneto@metrobank.plc,uk>
2023-10-16 16:16:20 +0100

    update lab04_part1

--- lab04_part_1.py
+++ lab04_part_1.py
@@ -5,7 +5,8 @@ ages = [12,18,33,84,45,67,12,82,95,16,10,23,43,29,40,34,30,16,44,69,70,74,38,65,
         19,59,7,44,93,35,72,25,63,11,69,11,76,10,60,30,14,21,82,47,6,21,88,46,78,92,
         48,36,28,51]
 
-#print(len(ages))
+#lenght = len(ages)
+#print(lenght)
 
 # part 2
 
@@ -15,16 +16,40 @@ ages = [12,18,33,84,45,67,12,82,95,16,10,23,43,29,40,34,30,16,44,69,70,74,38,65,
 # part 3
 
 #for i in range(len(ages)):
- #   ages[i] += 1
-  #  print(ages[i])
+  #  ages[i] += 1
+    #print(ages[i])
 
 # part 4
 
-i = 0
-while i < len(ages):
-    if ages[i] < 16 or ages[i] > 65:
-        del ages[i]
-    else:
-        i += 1
-print(ages)
+#i = 0
+#while i < len(ages):
+    #if ages[i] < 16 or ages[i] > 65:
+        #del ages[i]
+    #else:
+   #    i += 1
+#print(ages)
 
+# part 5
+
+#count = 0
+
+#for age in ages:
+#    if age >=16 and age <=25:
+#        count += 1
+    
+# part 6
+
+#ages.sort()
+#for age in ages:
+#    print(age)
+
+# part 7
+
+#word = input("enter a word: ")
+#vowels = "aeiou"
+#count = 0
+
+#for letter in word:
+ #   if letter.lower() in vowels:
+ #       count += 1
+#print(count) 
