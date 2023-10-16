93de272e0e3255973296761704e9b7b57703628b
TelmoLewis <124844690+TelmoLewis@users.noreply.github.com>
2023-10-16 13:50:28 +0100

    Update squares.py

--- squares.py
+++ squares.py
@@ -2,7 +2,7 @@ number = 1
 
 while number <= 100:
     square = number ** 2
-     if square > 2000:
+    if square > 2000:
         break
          
     print(f"Number: {number}, Square: {square}")
