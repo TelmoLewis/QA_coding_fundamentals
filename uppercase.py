fe9cc4b51ea0ada4884d4e4ea3330c534e48d637
telmo <telmo.lewisneto@metrobank.plc,uk>
2023-10-17 14:13:42 +0100

    add uppercase.py

---
+++ uppercase.py
@@ -0,0 +1,8 @@
+def convert_uppercase():
+    user_input = input("enter a word: ")
+    uppercase = user_input.upper()
+    return uppercase
+
+result = convert_uppercase()
+print("uppercase word is:",result)
+
