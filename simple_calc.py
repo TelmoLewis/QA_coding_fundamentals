dde33f4c8f92b607f503baadb77d35fa168b2969
telmo <telmo.lewisneto@metrobank.plc,uk>
2023-10-17 13:53:02 +0100

    add simple_calc.py

---
+++ simple_calc.py
@@ -0,0 +1,12 @@
+def operations():
+    
+    num1 = float(input("enter first number: "))
+    num2 = float(input("enter second number: "))
+
+    sum_result = num1 + num2
+    sub_result = num1 - num2
+    mul_result = num1 * num2
+
+    print(f"Sum is {sum_result}, sub is {sub_result} and multiply is {mul_result}")
+
+operations()
