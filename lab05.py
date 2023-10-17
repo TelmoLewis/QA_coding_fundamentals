fd183f4e4c4bbce0a79cac402d2fa9d7c92bb5f7
TelmoLewis <124844690+TelmoLewis@users.noreply.github.com>
2023-10-17 14:52:14 +0100

    Update lab05.py

--- lab05.py
+++ lab05.py
@@ -1,5 +1,5 @@
 data = "100,30,53,67,85,87,50,45,51,72,64,69,59,17,22,23,44,25,16,67,85,87,50,45,51,72,59,14,50,55,32,23,24,25,37,28,39,30,33,35,40,34,41,43,94,95,59,98,99,44,45,47,48,49,53,61,63,69,75,77,60,83"
-
+import statistics
 grades = data.split(",")
 grades = list(map(int, grades))
 
@@ -12,15 +12,16 @@ average_grade = sum(grades) / len(grades)
 # Round the average to 2 decimal places
 average_grade = round(average_grade, 2)
 
-print("Average grade: {:.2f}".format(average_grade))
+print("Average grade is:", average_grade)
 
-import statistics
+
 
 # Calculate the mean (average) using the statistics.mean() function
-average_grade = statistics.mean(grades)
+mean_grade = statistics.mean(grades)
 
 # Calculate the median using the statistics.median() function
 median_grade = statistics.median(grades)
+print("Median grade is:", median_grade)
 
 # Calculate the minimum and maximum
 min_grade = min(grades)
