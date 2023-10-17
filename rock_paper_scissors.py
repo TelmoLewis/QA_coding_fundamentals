3efebef202a628852a5b4b99bcc72123db126c36
TelmoLewis <124844690+TelmoLewis@users.noreply.github.com>
2023-10-17 20:03:19 +0100

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,23 +2,23 @@ import random
 
 def get_user_choice():
     while True:
-        user_choice = input("choose Rock, Paper or Scissors: ").strip().lower()
+        user_choice = input("Choose Rock, Paper, or Scissors: ").strip().lower()
         if user_choice in ["rock", "paper", "scissors"]:
             return user_choice
         else:
-            print("Invalid choice!!! Please choose rock, paper or scissors")
+            print("Invalid choice!!! Please choose rock, paper, or scissors")
 
 def get_computer_choice():
-   return random.choice(["rock", "paper", "scissors"])
+    return random.choice(["rock", "paper", "scissors"])
 
 def determine_winner(user_choice, computer_choice):
-   outcomes = {("rock", "scissors"), ("paper", "rock"), ("scissors", "paper")}
-   if user_choice == computer_choice:
-       return "Its a draw!!!"
-   elif (user_choice, computer_choice) in outcomes:
-       return "You win!!!"
-   else:
-       print("Computer Wins!!!")
+    outcomes = {("rock", "scissors"), ("paper", "rock"), ("scissors", "paper")}
+    if user_choice == computer_choice:
+        return "It's a draw!!!"
+    elif (user_choice, computer_choice) in outcomes:
+        return "You win!!!"
+    else:
+        return "Computer Wins!!!"
 
 user_wins = 0
 computer_wins = 0
@@ -29,14 +29,14 @@ while True:
     user_choice = get_user_choice()
     computer_choice = get_computer_choice()
     print(f"You choose {user_choice}")
-    print(f"Computer choose {computer_choice}")
+    print(f"Computer chooses {computer_choice}")
 
     result = determine_winner(user_choice, computer_choice)
     print(result)
 
-    if "You win!!!" in result:
+    if result == "You win!!!":
         user_wins += 1
-    elif "Computer wins!!!" in result:
+    elif result == "Computer Wins!!!":
         computer_wins += 1
     else:
         draw += 1
