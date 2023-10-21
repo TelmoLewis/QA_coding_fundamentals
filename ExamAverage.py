199a0614d421ed76f94d69fa66dc6a4bd0f793e7
TelmoLewis <124844690+TelmoLewis@users.noreply.github.com>
2023-10-21 21:52:14 +0100

    Update ExamAverage.py

--- ExamAverage.py
+++ ExamAverage.py
@@ -2,33 +2,26 @@ math_mark = -1
 english_mark = -1
 ict_mark = -1
 
-while math_mark >= 0 or math_mark <= 100:
-    math_mark = int(input("enter your maths mark between 0 and 100: "))
+while math_mark < 0 or math_mark > 100:
+    math_mark = int(input("Enter a Maths grade between 1 an 100: "))
     if math_mark < 0 or math_mark > 100:
-        print("Invalid input, marks should be between 1 and 100")
-    
+        print("Invalid mark, please enter a mark between 1 and 100")
 
-while english_mark >= 0 or english_mark <= 100:
-    english_mark = int(input("enter your english mark between 0 and 100: "))
+while english_mark < 0 or english_mark > 100:
+    english_mark = int(input("Enter a english mark between 1 and 100: "))
     if english_mark < 0 or english_mark > 100:
-        print("Invalid input, marks should be between 1 and 100")
- 
-        
-while ict_mark >= 0 or ict_mark <= 100:
-    ict_mark = int(input("enter your ict mark between 0 and 100: "))
-    if ict_mark < 0 or ict_mark > 100:
-        print("Invalid input, marks should be between 1 and 100")
+        print("Invalid mark, please enter a mark between 1 and 100")
 
- 
-average_mark = (math_mark + english_mark + ict_mark) /3
+while ict_mark < 0 or ict_mark > 100:
+    ict_mark = int(input("Enter a ICT mark between 1 and 100: "))
+    if ict_mark < 0 or ict_mark >100:
+        print("Invalid mark, please enter a mark between 1 and 100")
 
-print(f"average_mark: {average_mark:.2f}")
+average_mark = (math_mark + english_mark + ict_mark) / 3
+
+print(f"Average mark is: {average_mark:.2f}")
 
 if average_mark >= 65:
     print("Pass")
-
-else:
+elif average_mark < 65:
     print("Fail")
-
-
-
